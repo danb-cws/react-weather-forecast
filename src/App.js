a5cc2dc7ebe91dd08b257ea58731acf537a65e21
danb-cws <sugarlumpltd@gmail.com>
2016-12-08 00:25:52 +0000

    deleting unused

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import * as config from './config';
 import Weather from './Weather';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -9,7 +8,6 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1>{config.forecastTotalDays}-day weather forecast for {config.cityName}, {config.countryCode.toUpperCase()}</h1>
         </div>
         <Weather />
