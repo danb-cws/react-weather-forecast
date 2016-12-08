f61c09bd1f080e1bc629da1d02e9c5a4b84b01a1
danb-cws <sugarlumpltd@gmail.com>
2016-12-08 00:25:07 +0000

    Weather component - individual weather items

--- src/Weather.js
+++ src/Weather.js
@@ -19,15 +19,22 @@ class Weather extends Component {
       .then(response => response.json())
       .then(responseJson => {
         this.weatherList = responseJson.list;
+        // console.log(this.weatherList);
         this.setState({data: responseJson.list[0].weather[0].description});
       })
       .catch((error) => {
-        console.error(error);
+        // console.error(error);
       });
   }
   render() {
-    let weatherDaily = this.weatherList.slice(0, config.forecastTotalDays).map((dayNum, index) => {
-      return <li key={index}>Day {++index}: {dayNum.weather[0].description}</li>;
+    let weatherDaily = this.weatherList.slice(0, config.forecastTotalDays*8).map((dayNum, index) => {
+      const windDirectionPointerStyle = {display: 'inline-block', transform: 'rotate(' + dayNum.wind.deg + 'deg)'};
+      return <li key={index}>
+          <div className="date">{dayNum.dt_txt.substring(5, 16)}</div>
+          <div className="description">{dayNum.weather[0].description}</div>
+          <div className="temp">{Math.round(dayNum.main.temp - 273.15)}&deg;C</div>
+          <div className="wind">Wind: {Math.round(dayNum.wind.speed)}m/sec  <span style={windDirectionPointerStyle}>&uarr;</span></div>
+        </li>;
     });
     return (
       <ul className="App-forecast">
